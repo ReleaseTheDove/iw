952b218bf7a0009b6a0e5d37eba7dffeb9f58ca9
zdk <coutingstars1@163.com>
2021-02-21 19:22:26 +0800

    cros fix

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from app.resources import router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def create_app():
     app = FastAPI()
 
+    origins = [
+        'http://localhost:8080',
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins = origins,
+    )
 
     app.include_router(router, prefix='/api')
 
